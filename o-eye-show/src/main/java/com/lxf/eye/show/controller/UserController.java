62950c4c14ffce6fc912706fc03b6a5d82f9d1e7
life <lxf_life@163.com>
2019-09-28 17:37:36 +0800

    添加dubbo

--- o-eye-show/src/main/java/com/lxf/eye/show/controller/UserController.java
+++ o-eye-show/src/main/java/com/lxf/eye/show/controller/UserController.java
@@ -1,30 +1,19 @@
 package com.lxf.eye.show.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.lxf.eye.common.domain.OrderInfo;
 import com.lxf.eye.common.domain.User;
-import com.lxf.eye.common.domain.api.OrderServiceDubbo;
-import com.lxf.eye.show.conf.EyeShowConf;
-import com.lxf.eye.show.service.IndexService;
 import com.lxf.eye.show.service.UserService;
-import com.xxl.conf.core.annotation.XxlConf;
-import io.swagger.models.auth.In;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class UserController {
     @Resource
     private UserService userService;
-    @Resource
-    private EyeShowConf eyeShowConf;
 
     @RequestMapping("/queryUser")
     @ResponseBody
@@ -38,11 +27,5 @@ public class UserController {
     public String queryUserAll() {
         List<User> users = userService.queryUserAll();
         return JSON.toJSONString(users);
-    }
-    @RequestMapping("/queryVersion")
-    @ResponseBody
-    public String queryVersion() {
-        System.out.printf("version:"+eyeShowConf.version);
-        return JSON.toJSONString(eyeShowConf.version);
     }
 }
--- o-eye-show/src/test/java/com/lxf/eye/show/OEyeApplicationTests.java
+++ o-eye-show/src/test/java/com/lxf/eye/show/OEyeApplicationTests.java
@@ -1,10 +1,8 @@
 package com.lxf.eye.show;
 
-import com.lxf.eye.show.conf.EyeShowConf;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
